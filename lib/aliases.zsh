326985638d94fc546c7c97150589e12dcb7d82c1
stardiviner <numbchild@gmail.com>
2012-08-10 12:30:35 +0800

    add directory alias ..N

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -4,6 +4,10 @@ alias po='popd'
 
 # Basic directory operations
 alias ...='cd ../..'
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
 alias -- -='cd -'
 
 # Super user
