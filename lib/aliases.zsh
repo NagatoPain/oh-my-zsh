ced0fce486c69a3ce49f32cb4ed08ed69dfed88a
stardiviner <numbchild@gmail.com>
2012-08-11 08:17:44 +0800

    merge legacy .zsh aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -15,17 +15,39 @@ alias _='sudo'
 
 # Grep
 #alias g='grep -in'
-#alias grep='grep --color=auto'
 #alias grep.ack='ack-grep -il'
 
 # Show history
 alias history='fc -l 1'
 
 # List direcory contents
-alias lsa='ls -lah'
-alias l='ls -la'
+alias l='ls++'
 alias ll='ls -l'
-alias sl=ls # often screw this up
+alias lsa='ls -lah'
+# alias ls='ls --color=always -N -B -h -F'
+# alias pwd='pwd -P' # use '-P' option to avoid symbol links.
+alias subdir='ls -F | grep /$' # list out all subdirectories.
 
-alias afind='ack-grep -il'
+# Verbose File manipulation
+alias rm='rm -i -v'
+# alias mv='mv -v -i -b -S".bak" -u'
+# alias cp='cp -i -b -S".bak" -u -v --recursive'
+alias mv='mv -i -b'
+alias cp='cp -i -b -R -H'
+alias chattr='chattr -V'
+alias rename='rename -v'
+alias rmdir='rmdir -v'
+alias mkdir='mkdir -v'
+alias eject='eject -v'
 
+# Disk manipulation
+alias df='df -a -h -T'
+
+# Process
+alias ps_visual='ps aux --forest | sort -nk +4 | tail'
+alias ps_tops='ps aux | sort -nrk +4 | head'
+
+# Admin
+alias chmod='chmod -c'
+alias chown='chown -c'
+alias chgrp='chgrp -c'
